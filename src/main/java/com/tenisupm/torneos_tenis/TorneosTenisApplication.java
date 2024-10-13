10aeb60960ef058fbf749d2239cd5ea929d80a6f
DiegoMenda <diegomendanaalvarez@gmail.com>
2024-10-13 13:45:22 +0200

    Commit inical

---
+++ src/main/java/com/tenisupm/torneos_tenis/TorneosTenisApplication.java
@@ -0,0 +1,13 @@
+package com.tenisupm.torneos_tenis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorneosTenisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorneosTenisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenisupm/torneos_tenis/TorneosTenisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenisupm.torneos_tenis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorneosTenisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
