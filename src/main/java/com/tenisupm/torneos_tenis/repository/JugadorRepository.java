ea69a58a6ef13a7cefe8a6123067fa3537cb6d41
DiegoMenda <diegomendanaalvarez@gmail.com>
2024-10-15 00:44:13 +0200

    DELETE y PUT, primera versión a modo de plantilla añadidos para Jugador

--- src/main/java/com/tenisupm/torneos_tenis/repository/JugadorRepository.java
+++ src/main/java/com/tenisupm/torneos_tenis/repository/JugadorRepository.java
@@ -1,11 +1,16 @@
 package com.tenisupm.torneos_tenis.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.tenisupm.torneos_tenis.entity.Jugador;
 
 @Repository
 public interface JugadorRepository extends JpaRepository<Jugador, Long> {
-
+	
+	@Query("SELECT j FROM Jugador j WHERE j.username = ?1")
+	Optional<Jugador> findJugadorByUsername(String username);
 }
